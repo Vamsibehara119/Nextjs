e5abde092593074188dd361972d82b9eae98ae5c
BalajiUggina <ugginabalaji2003@gmail.com>
2026-01-08 21:24:52 +0530

    added reset button in fallback ui

--- ErrorBoundary/FallbackUI.tsx
+++ ErrorBoundary/FallbackUI.tsx
@@ -3,14 +3,17 @@
 import { useError } from "./ErrorContext";
 
 export default function FallbackUI({ area }: { area?: string }) {
-  const err = useError();
+  const {error ,reset} = useError();
 
   return (
     <div className="p-4 bg-red-100 text-red-800 text-sm">
       <strong>{area} failed</strong>
       <pre className="text-xs mt-1 whitespace-pre-wrap">
-        {err?.error?.message}
+        {error?.message}
       </pre>
+      <button onClick={reset} 
+       className="mt-2 rounded bg-red-600 px-3 py-1 text-white text-xs"
+      >Retry</button>
     </div>
   );
 }
